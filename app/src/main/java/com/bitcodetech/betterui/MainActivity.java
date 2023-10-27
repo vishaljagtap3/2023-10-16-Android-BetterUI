6fd347a72a038527f615a06df3979b784b5af44a
vishal <vishal.jagtap@bitcode.in>
2023-10-27 10:32:08 +0530

    Creating UI through xml and adding views dynamically

--- app/src/main/java/com/bitcodetech/betterui/MainActivity.java
+++ app/src/main/java/com/bitcodetech/betterui/MainActivity.java
@@ -1,11 +1,13 @@
 package com.bitcodetech.betterui;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -16,12 +18,13 @@ public class MainActivity extends Activity {
     private TextView txtWelcomeMessage;
     private EditText edtWelcomeMessage;
     private Button btnSetMessage;
-
+    private LinearLayout mainContainer;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        mainContainer = findViewById(R.id.mainContainer);
         imgLogo = findViewById(R.id.imgLogo);
         txtWelcomeMessage = findViewById(R.id.txtWelcomeMessage);
         edtWelcomeMessage = findViewById(R.id.edtWelcomeMessage);
@@ -32,6 +35,20 @@ public class MainActivity extends Activity {
                     @Override
                     public void onClick(View view) {
                         txtWelcomeMessage.setText(edtWelcomeMessage.getText().toString());
+                    }
+                }
+        );
+
+        imgLogo.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        TextView txtName = new TextView(MainActivity.this);
+                        txtName.setTextSize(30);
+                        txtName.setTextColor(Color.BLACK);
+                        txtName.setText("Name goes here");
+
+                        mainContainer.addView(txtName);
                     }
                 }
         );
